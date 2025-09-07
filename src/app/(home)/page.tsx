2afa34792266df6275becc8932c597a82ba7379b
Gaubongtoi <tanvndn123@gmail.com>
2025-09-07 22:14:05 +0700

    Setup project

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image';
 
 // Home Component
+// Khi bạn đặt tên cho folder là (home) thì file page.tsx bên trong folder đó sẽ trở thành trang chủ của ứng dụng
+// Tương tự như vậy, nếu bạn đặt tên folder là (dashboard) thì file page.tsx bên trong folder đó sẽ trở thành trang dashboard của ứng dụng
+// Lưu ý: Bạn chỉ nên có một folder được đặt tên là (home) trong toàn bộ ứng dụng của bạn
+// Nếu bạn cần nhiều trang chủ khác nhau, hãy sử dụng các route khác nhau thay vì tạo nhiều folder (home)
 export default function Home() {
     return (
         <div>
