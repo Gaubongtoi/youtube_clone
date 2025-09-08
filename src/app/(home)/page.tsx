5fde0531131f368a2080501b887ae82447536cbd
Gaubongtoi <tanvndn123@gmail.com>
2025-09-08 17:07:53 +0700

    Setup Layout + Adding clerk component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,8 @@
-import Image from 'next/image';
-
 // Home Component
 // Khi bạn đặt tên cho folder là (home) thì file page.tsx bên trong folder đó sẽ trở thành trang chủ của ứng dụng
 // Tương tự như vậy, nếu bạn đặt tên folder là (dashboard) thì file page.tsx bên trong folder đó sẽ trở thành trang dashboard của ứng dụng
 // Lưu ý: Bạn chỉ nên có một folder được đặt tên là (home) trong toàn bộ ứng dụng của bạn
 // Nếu bạn cần nhiều trang chủ khác nhau, hãy sử dụng các route khác nhau thay vì tạo nhiều folder (home)
 export default function Home() {
-    return (
-        <div>
-            {/* NextJS khai báo đường dẫn cho logo sẽ khác '/logo.svg' -> trỏ trực tiếp tới folder public*/}
-            <Image src="/logo.svg" height={50} width={50} alt="logo" />
-            <p className="text-xl font-semibold tracking-tight">Youtube</p>
-        </div>
-    );
+    return <div>I will load videos in the future</div>;
 }
--- src/modules/home/ui/components/home-sidebar/index.tsx
+++ src/modules/home/ui/components/home-sidebar/index.tsx
@@ -1,15 +1,17 @@
 import { Separator } from '@/components/ui/separator';
 import { Sidebar, SidebarContent } from '@/components/ui/sidebar';
 import { MainSection } from '@/modules/home/ui/components/home-sidebar/main-section';
+import { PersonalSection } from './personal-section';
 
 export const HomeSidebar = () => {
     return (
         <div>
-            <Sidebar className="pt-16 z-40 border-none">
+            <Sidebar className="pt-16 z-40 border-none" collapsible="icon">
                 <SidebarContent className="bg-background">
                     <MainSection />
+                    {/* ------------------- */}
                     <Separator />
-                    <MainSection />
+                    <PersonalSection />
                 </SidebarContent>
             </Sidebar>
         </div>
